07b134a6c313b0531326d17839dd265589399cd5
James Oliver <jorlabor.mugna@gmail.com>
2022-11-15 01:45:33 +0800

    Add svg

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,19 +2,48 @@ import React from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card, Paragraph } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
   backgroundcolor: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.sizes[1]};
+  padding: ${(props) => props.theme.space[3]};
   backgroundcolor: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Title = styled.Text`
-  padding: ${(props) => props.theme.sizes[1]};
+  font-family: ${(props) => props.theme.fonts.heading}
+  font-size: ${(props) => props.theme.fontSizes.body}
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Rating = styled.View`
+  padding-vertical: ${(props) => props.theme.space[2]};
+  flex-direction: row;
+`;
+
+const Section = styled.View`
+  align-items: center;
+  flex-direction: row;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex=direction: row;
+  justify-content: flex-end;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -26,17 +55,29 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     ],
     address = "100 some random street",
     isOpenNow = true,
-    rating = 3,
+    rating = 4,
     isClosedTemporarily,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
-    <RestaurantCard>
-      <RestaurantCardCover source={{ uri: photos[0] }} />
-      <Card.Title title={name} subtitle="Card Subtitle" />
-      <Title>{name}</Title>
-      <Card.Content>
-        <Paragraph>{address}</Paragraph>
-      </Card.Content>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map((item, index) => (
+              <SvgXml key={index} xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+          </SectionEnd>
+        </Section>
+
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
